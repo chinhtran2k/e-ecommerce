98f592f6989381ce7247f07370d150da88323d58
chinhtran2k <chinhtran2k000@gmail.com>
2022-01-04 15:15:17 +0700

    first commit

---
+++ EC.ServiceClient/Interfaces/ICategoryApiClient.cs
@@ -0,0 +1,14 @@
+﻿using EC.ViewModel.Categories;
+using EC.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EC.ServiceClient.Interfaces
+{
+    public interface ICategoryApiClient
+    {
+        Task<ApiResult<List<CategoryViewModel>>> GetAll();
+    }
+}
---
+++ WebAppMVC/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using EC.ServiceClient.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductApiClient _productApiClient;
+        private readonly ICategoryApiClient _categoryApiClient;
+
+        public ProductController(IProductApiClient productApiClient, ICategoryApiClient categoryApiClient)
+        {
+            _productApiClient = productApiClient;
+            _categoryApiClient = categoryApiClient;
+        }
+       
+    }
+}
